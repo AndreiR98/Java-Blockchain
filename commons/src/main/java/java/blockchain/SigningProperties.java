4e9ff6c6af35466352cd6ab370b007b59eec2fef
Randrei <rotaru.andrei98@gmail.com>
2022-06-17 16:36:07 +0100

    chore:add commons

---
+++ commons/src/main/java/java/blockchain/SigningProperties.java
@@ -0,0 +1,4 @@
+package java.blockchain;
+
+public class SigningProperties {
+}
---
+++ commons/src/main/java/java/blockchain/math/hexToInt.java
@@ -0,0 +1,8 @@
+package java.blockchain.math;
+
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigInteger;
+
+public class hexToInt {
+}
